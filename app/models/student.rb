f5b4ca64454259fea5809d7c46161e7047a88b70
alukima <amylukima@gmail.com>
2013-05-29 16:55:32 -0700

    Finished

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,29 @@
 require_relative '../../db/config'
+require 'date'
 
-# implement your Student model here
+class Student < ActiveRecord::Base
+  validates :email, :format => { :with => /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/,
+    :message => "Must have valid email address" }
+  validates :email, :uniqueness => true
+  validates :age, :numericality => { :greater_than => 5 }
+  validate :phone_number_has_ten_digits_or_more
+
+  def phone_number_has_ten_digits_or_more
+    if phone.gsub!(/\D/, "").split('').count < 10
+      errors.add(:phone_num, "Must have at least 10 digits")
+    end
+  end
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    if birthday.month < Date.today.month
+      Date.today.year - birthday.year
+    else
+      Date.today.year - birthday.year - 1
+    end
+  end
+
+end
