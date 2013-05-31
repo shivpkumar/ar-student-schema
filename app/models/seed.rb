0c9083334da7ce302fa1b61c93aef5d11ae48c6d
alukima <amylukima@gmail.com>
2013-05-30 23:20:51 -0700

    Added name field in students table

--- app/models/seed.rb
+++ app/models/seed.rb
@@ -1,22 +1,22 @@
 require_relative 'teacher'
 require_relative 'student'
 
-10.times do
-  Teacher.create :name => Faker::Name.name,
-                 :email => Faker::Internet.email,
-                 :phone => Faker::PhoneNumber.phone_number
-end
+# 10.times do
+#   Teacher.create :name => Faker::Name.name,
+#                  :email => Faker::Internet.email,
+#                  :phone => Faker::PhoneNumber.phone_number
+# end
 
-100.times do
-  Student.create  :first_name => Faker::Name.name,
-                  :last_name => Faker::Name.name,
-                  :birthday => Date.new(1980,10,23),
-                  :gender => ['male','female'].sample,
-                  :email => Faker::Internet.email,
-                  :phone => Faker::PhoneNumber.phone_number,
-                  :created_at => DateTime.now,
-                  :updated_at => DateTime.now
-end
+# 100.times do
+#   Student.create  :first_name => Faker::Name.first_name,
+#                   :last_name => Faker::Name.last_name,
+#                   :birthday => Date.new(1980,10,23),
+#                   :gender => ['male','female'].sample,
+#                   :email => Faker::Internet.email,
+#                   :phone => Faker::PhoneNumber.phone_number,
+#                   :created_at => DateTime.now,
+#                   :updated_at => DateTime.now
+# end
 
 # teacher = Teacher.find(3)
 # stud = Student.find(55)
@@ -30,7 +30,12 @@ end
 
 # Student.find(5).teachers << teacher
 
-1.upto(100) do |x|
-  rand_num = (1..10).to_a.sample 
-  Student.find(x).teachers << Teacher.find(rand_num)
+# 1.upto(100) do |x|
+#   rand_num = (1..10).to_a.sample 
+#   Student.find(x).teachers << Teacher.find(rand_num)
+# end
+
+Student.all.each do |student|
+  student.name = "#{student.first_name} #{student.last_name}"
+  student.save
 end
